0c9d97a627d9a78bb0c1ac6021aa7f2b905ea645
jacknorris752 <jackrobertnorris752@gmail.com>
2019-06-02 04:50:52 +0100

    -Units now find their closest target and store it to memory

--- FactionSimulator/src/Unit.java
+++ FactionSimulator/src/Unit.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Unit {
 	
@@ -14,6 +15,7 @@ public class Unit {
 	private int myX,myY;
 	private int moveRate; //how fast a unit can move per turn probably faction based?
 	public static final int size = 20;
+	private final int stepSize = 5;
 	
 	private boolean alive = true;
 	
@@ -28,13 +30,41 @@ public class Unit {
 	}
 	
 	public void update() {
-		//if target is null then run search
+		if(target == null) {
+			search();
+		}
 		//movement in here
+		//my values go to target values in increments of "stepSize"
+		System.out.println("I am at: " + this.myX + ", " + this.myY +
+				"\n MY target is at: " + target.x + ", " + target.y);
+		
 	}
 	
-	//DO ME FIRST
 	public void search() {
 		//set target to closest town from Map.getTowns();
+		
+		ArrayList<Town> temp = Map.getTowns();	//list of towns
+		Town closest = null;		//temporary closest value
+		double closestDistance = 0;
+		
+		
+		if(closest == null) {
+			double distance = Engine.distance(this.myX, this.myY, temp.get(0).x,temp.get(0).y);
+			closest = temp.get(0);
+			closestDistance = distance;
+		}
+		
+		
+		for(int i = 0; i < temp.size(); i++) {
+			double distance = Engine.distance(this.myX, this.myY, temp.get(i).x,temp.get(i).y);
+
+			if(distance < closestDistance) {
+				closest = temp.get(i);
+				closestDistance = distance;
+			}
+		}
+		
+		target = closest;
 	}
 	
 	
